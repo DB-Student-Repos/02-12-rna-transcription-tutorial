b283153637505bfe8aa1c1ede0b1ef434c3a057d
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-12-07 22:34:11 +0100

    Update rna_transcription.py

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,2 +1,14 @@
 def to_rna(dna_strand):
-    pass
+    rna = ""
+    for ch in dna_strand:
+        if ch == 'A':
+            rna = rna + 'U'
+        elif ch == 'T':
+            rna = rna + 'A'
+        elif ch == 'C':
+            rna = rna + 'G'
+        elif ch == 'G':
+            rna = rna + 'C'
+    return rna
+            
+        
